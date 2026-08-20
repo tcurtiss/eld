300c03ba8f482e7bc3a95b5afadc95ee4b9407f6
Troy Curtiss <trcurtiss@gmail.com>
2026-08-19 18:13:23 -0600

    Add lit test for --time-region scoping of --plugin-config YAML plugins
    Covers Module::readOnePluginConfig()'s three-way OR (bare "Plugin",
    scoped plugin name, or all-user-plugins) that gates PrintTimingStats
    for plugins registered via a --plugin-config YAML GlobalPlugins entry.
    
    Signed-off-by: Troy Curtiss <trcurtiss@gmail.com>

---
+++ test/Common/Plugin/PluginConfigTimingStats/PluginConfigTimingStatsPlugin.cpp
@@ -0,0 +1,33 @@
+#include "Defines.h"
+#include "PluginVersion.h"
+#include "LinkerPlugin.h"
+#include <iostream>
+
+class DLL_A_EXPORT PluginConfigTimingStatsPlugin : public eld::plugin::LinkerPlugin {
+public:
+  PluginConfigTimingStatsPlugin()
+      : eld::plugin::LinkerPlugin("PluginConfigTimingStatsPlugin") {}
+
+  void Init(const std::string &options) override {
+    std::cout << "Init called with options: " << options << "\n";
+  }
+};
+
+eld::plugin::PluginBase *ThisPlugin = nullptr;
+
+extern "C" {
+bool DLL_A_EXPORT RegisterAll() {
+  ThisPlugin = new PluginConfigTimingStatsPlugin();
+  return true;
+}
+
+eld::plugin::PluginBase DLL_A_EXPORT *getPlugin(const char *T) {
+  return ThisPlugin;
+}
+
+void DLL_A_EXPORT Cleanup() {
+  if (ThisPlugin)
+    delete ThisPlugin;
+  ThisPlugin = nullptr;
+}
+}
